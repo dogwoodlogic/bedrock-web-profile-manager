188bbe8028e601b5c5cab062cc4b69625673ad50
Matthew Collier <mcollier@digitalbazaar.com>
2020-04-21 14:22:00 -0400

    Add lint script.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
+  root: true,
   env: {
     browser: true
   },
-  extends: ['digitalbazaar', 'digitalbazaar/jsdoc']
+  extends: ['digitalbazaar', 'digitalbazaar/jsdoc'],
+  ignorePatterns: ['node_modules/']
 }
