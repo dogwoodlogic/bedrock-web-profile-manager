d17c6e8275d00cdd1d307017ed142646c9f2cb49
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2020-09-04 13:37:52 -0400

    Remove `axios` and use `httpClient` instead.
    - Add assertions.

--- keyResolver.js
+++ keyResolver.js
@@ -3,15 +3,18 @@
  */
 'use strict';
 
-import axios from 'axios';
+const {httpClient} = require('@digitalbazaar/http-client');
+const assert = require('assert-plus');
 
 async function keyResolver({id, didMethod = 'key'} = {}) {
+  assert.string(id, 'id');
+  assert.string(didMethod, 'didMethod');
   const SUPPORTED_DID_METHODS = ['key', 'v1'];
   if(!SUPPORTED_DID_METHODS.includes(didMethod)) {
     throw new Error(`Unsupported DID method "${didMethod}".`);
   }
   const headers = {Accept: 'application/ld+json, application/json'};
-  const response = await axios.get(id, {
+  const response = await httpClient.get(id, {
     headers
   });
   return response.data;
