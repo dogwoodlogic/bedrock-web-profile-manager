3276903f78c1f5b2a019bb44e8245a9fd9d8d764
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2020-09-21 16:28:44 -0400

    Use did-io to resolve did keys.

--- keyResolver.js
+++ keyResolver.js
@@ -3,13 +3,19 @@
  */
 import {httpClient} from '@digitalbazaar/http-client';
 import assert from 'assert-plus';
+import didIo from 'did-io';
+import {driver as keyDriver} from 'did-method-key';
+import {driver as veresDriver} from 'did-method-key';
+
+// config did-io to support did:key and did:v1 drivers
+didIo.use('key', keyDriver());
+didIo.use('v1', veresDriver());
 
 async function keyResolver({id, didMethod = 'key'} = {}) {
   assert.string(id, 'id');
   assert.string(didMethod, 'didMethod');
-  const SUPPORTED_DID_METHODS = ['key', 'v1'];
-  if(!SUPPORTED_DID_METHODS.includes(didMethod)) {
-    throw new Error(`Unsupported DID method "${didMethod}".`);
+  if(id.startsWith('did:')) {
+    return didIo.get({did: id});
   }
   const headers = {Accept: 'application/ld+json, application/json'};
   const response = await httpClient.get(id, {
