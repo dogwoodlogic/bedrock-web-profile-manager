3b0e265e728255bce685de81f03d77a68abf08b7
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2020-09-22 15:15:08 -0400

    Restrict to hit a URL that begins with `origin`.

--- keyResolver.js
+++ keyResolver.js
@@ -5,23 +5,28 @@ import {httpClient} from '@digitalbazaar/http-client';
 import assert from 'assert-plus';
 import didIo from 'did-io';
 import {driver as keyDriver} from 'did-method-key';
-import {driver as veresDriver} from 'did-method-key';
+import {driver as veresDriver} from 'did-veres-one';
 
 // config did-io to support did:key and did:v1 drivers
 didIo.use('key', keyDriver());
 didIo.use('v1', veresDriver());
 
-async function keyResolver({id, didMethod = 'key'} = {}) {
+async function keyResolver({id} = {}) {
   assert.string(id, 'id');
-  assert.string(didMethod, 'didMethod');
   if(id.startsWith('did:')) {
-    return didIo.get({did: id});
+    return didIo.get({did: id, forceConstruct: true});
   }
-  const headers = {Accept: 'application/ld+json, application/json'};
-  const response = await httpClient.get(id, {
-    headers
-  });
-  return response.data;
+  const origin = `${location.protocol}//${location.hostname}`;
+  if(id.startsWith(origin)) {
+    const headers = {Accept: 'application/ld+json, application/json'};
+    const response = await httpClient.get(id, {
+      headers
+    });
+    return response.data;
+  }
+  throw TypeError(
+    `"id" must start with either "did:" or ${origin}.`
+  );
 }
 
 export default keyResolver;
