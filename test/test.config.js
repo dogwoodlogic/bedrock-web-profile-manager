b1484e3910084d6e1039a4223e88a53623de3bdf
Dave Longley <dlongley@digitalbazaar.com>
2020-09-25 17:59:54 -0400

    Update to use latest KMS config vars.

--- test/test.config.js
+++ test/test.config.js
@@ -41,15 +41,8 @@ config.mongodb.dropCollections.collections = [];
 // allow self-signed certs in test framework
 config['https-agent'].rejectUnauthorized = false;
 
-config['profile-http'].kms.public.host = 'localhost';
-config['profile-http'].kms.public.port = 9876;
-config['profile-http'].kms.private.host = 'localhost';
-config['profile-http'].kms.private.port = 9876;
-
 // do not require an authentication session for tests
 config['kms-http'].requireAuthentication = false;
 
-config.kms.allowedHost = `${config['profile-http'].kms.public.host}:` +
-  `${config['profile-http'].kms.public.port}`;
-
 config.server.host = 'localhost:9876';
+config.kms.allowedHost = config.server.host;
