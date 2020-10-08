a551cbefbf98330aa62ed7ce35ef74bc9d38ce3a
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2020-10-08 14:45:29 -0400

    Remove assert-plus and add type check.

--- keyResolver.js
+++ keyResolver.js
@@ -1,18 +1,19 @@
 /*!
  * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
  */
-import {httpClient} from '@digitalbazaar/http-client';
-import assert from 'assert-plus';
 import didIo from 'did-io';
 import {driver as keyDriver} from 'did-method-key';
 import {driver as veresDriver} from 'did-veres-one';
+import {httpClient} from '@digitalbazaar/http-client';
 
 // config did-io to support did:key and did:v1 drivers
 didIo.use('key', keyDriver());
 didIo.use('v1', veresDriver());
 
 async function keyResolver({id} = {}) {
-  assert.string(id, 'id');
+  if(typeof id !== 'string') {
+    throw new TypeError('"id" string is required.');
+  }
   if(id.startsWith('did:')) {
     return didIo.get({did: id, forceConstruct: true});
   }
